f3d05e3a5656d3a064eadcf368dedb8655037b5c
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-22 13:50:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 #Method takes array of names and returns each name with a message
-def conference_badges(array)
+def batch_badge_creator(array)
   array.collect do |name|
     badge_maker(name)
   end
