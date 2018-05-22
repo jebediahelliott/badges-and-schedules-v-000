f2c474ce94d3cd3731bf96ce3de05bc7cc6be21d
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-22 14:01:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,9 @@ def batch_badge_creator(array)
     badge_maker(name)
   end
 end
+
+#Method takes array of names and assigns a room number to each
+def assign_rooms(array)
+  array.map.with_index do |name, i|
+    "Hello, #{name}! You'll be assigned to room #{i+1}!"
+end
